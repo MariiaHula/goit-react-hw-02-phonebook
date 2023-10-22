0134403ae36f17c1991580fcbf66554420ebccdf
MariiaHula <mariyagulaya@gmail.com>
2023-10-22 20:59:54 +0300

    add placeholder
    add placeholder

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Filter = ({ filteredContacts }) => {
-  return <input type="text" name="filter" onChange={filteredContacts} />;
+  return (
+    <input
+      type="text"
+      name="filter"
+      onChange={filteredContacts}
+      placeholder="Find contact by name"
+    />
+  );
 };
 
 export default Filter;
